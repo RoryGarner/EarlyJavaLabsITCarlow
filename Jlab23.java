e6a5e89af6e7202c2aa7873c15250d934d2298dd
RoryGarner <rorygarner79@me.com>
2016-09-29 10:30:14 +0100

    Check if a number is odd or even
    using modular divide

---
+++ Jlab23.java
@@ -0,0 +1,38 @@
+
+class Jlab23
+
+// Student Name : 		Rory Garner
+// Student Id Number : 	
+// Date :				17/9/14
+
+
+{
+    public static void main(String[] args)
+    {
+    	int value ;
+    	int answer ;
+    	
+    	// input
+    	System.out.print("Please enter a whole number - "); 
+    	value =  EasyIn.getInt() ;
+    	
+    	answer = value % 2; //using modular divide 
+    	
+    	
+        	if (answer ==0) 
+    		{
+    			System.out.println("This number is EVEN ");
+			}
+		else if (answer==1)
+			{
+				System.out.println("This number is ODD ");
+			}
+    	
+    	//calculation
+
+
+    }
+}
+
+
+	
