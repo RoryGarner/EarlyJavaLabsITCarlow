0f06a9da75b9522a8060241c153f9646dee83f25
RoryGarner <rorygarner79@me.com>
2016-09-29 10:23:48 +0100

    checks if number is greater than 0

---
+++ Jlab21.java
@@ -0,0 +1,29 @@
+
+class Jlab21
+
+// Student Name : 		Rory Garner
+// Student Id Number : 
+// Date :				17/9/14
+
+
+{
+    public static void main(String[] args)
+    {
+    	int value ;
+    	   	
+    	System.out.print("Please enter your number - "); 
+    	value =  EasyIn.getInt() ; 
+    	if (value > 0)// checks if number is greater than 0
+    		{	
+    			System.out.println("This number is greater than 0 ");
+	
+			}	
+		else if (value <=0)
+			{	
+    			System.out.println("This number is less than or equil to 0 ");
+	
+			}
+
+    }
+}
+
