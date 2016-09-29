d3d37c89802c79a6b1a059845239e47368598a8d
RoryGarner <rorygarner79@me.com>
2016-09-29 11:07:03 +0100

    Using a switch print the month
    Write a program that takes as input the number of a month and outputs the name of that month.

---
+++ Jlab2b5.java
@@ -0,0 +1,56 @@
+
+class Jlab2b5
+
+// Student Name : 		Rory Garner
+// Student Id Number : 	
+// Date :				17/9/14
+// Purpose : 			Get the average
+{
+    public static void main(String[] args)
+    {
+	int num1;
+	
+	System.out.println("Please enter number between 1-12 : "); 
+    num1 =  EasyIn.getInt() ; 
+    switch (num1)
+		{ 		
+				case 1:
+						System.out.println ("Jan");
+						break;
+				case 2:
+						System.out.println ("Feb");
+						break;
+				case 3:
+						System.out.println ("Mar");
+						break;
+				case 4:
+						System.out.println ("Apr");
+						break;
+				case 5:
+						System.out.println ("May");
+						break;
+				case 6:
+						System.out.println ("Jun");
+						break;
+				case 7:
+						System.out.println ("Jul");
+						break;
+				case 8:
+						System.out.println ("Aug");
+						break;
+				case 9:
+						System.out.println ("Sep");
+						break;
+				case 10:
+						System.out.println ("Oct");
+						break;
+				case 11:
+						System.out.println ("Nov");
+						break;
+				case 12:
+						System.out.println ("Dec");
+						break;
+	} // end switch
+   }
+}
+
