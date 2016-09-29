5499719ca5fb4adbcbc2e73d6c48d1977f262eef
RoryGarner <rorygarner79@me.com>
2016-09-29 10:27:38 +0100

    Check if a number is odd or even
    Using modular divide to check id a number is odd or even

---
+++ Jlab24.java
@@ -0,0 +1,39 @@
+
+class Jlab24
+
+// Student Name : 		Rory Garner
+// Student Id Number : 	C00193506
+// Date :				17/9/14
+// Purpose : 			
+
+
+
+
+{
+    public static void main(String[] args)
+    {
+    	int number1;
+    	int number2;
+    	
+    	
+    	System.out.println("Please enter the first number - "); 
+    	number1 =  EasyIn.getInt() ; 
+    	System.out.println("Please enter the second - "); 
+    	number2 =  EasyIn.getInt() ; 
+
+		if (number1 % number2 == 0)
+			{
+				System.out.println("The number is divisable - ");
+    		}
+    	else
+    		{
+				System.out.println("The number not is divisable - ");
+    		}
+		
+	
+	
+
+
+    }
+}
+
